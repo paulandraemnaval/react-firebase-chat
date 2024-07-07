56962e8fd31f662473a0ce647350ca99eb5fbb74
unknown <22-1-00759@vsu.edu.ph>
2024-07-08 00:31:23 +0800

    can now download pics

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyBLqVRigyEe8EBQUAXJFajVa5Ph0dhQDJA",
-  authDomain: "chat-4f995.firebaseapp.com",
-  projectId: "chat-4f995",
-  storageBucket: "chat-4f995.appspot.com",
-  messagingSenderId: "825020704040",
-  appId: "1:825020704040:web:a8f3f0bf4d63aa37489c1d",
-  measurementId: "G-B6E9N9BW3W",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "webdev-d00f8.firebaseapp.com",
+  databaseURL: "https://webdev-d00f8-default-rtdb.firebaseio.com",
+  projectId: "webdev-d00f8",
+  storageBucket: "webdev-d00f8.appspot.com",
+  messagingSenderId: "666391682027",
+  appId: "1:666391682027:web:e02c136ac3f10b405e97ae",
+  measurementId: "G-2WQ2Q1MPXP",
 };
 
 // Initialize Firebase
